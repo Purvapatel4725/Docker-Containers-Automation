05b3929ca324058282005cf757e0c1f263a8f3fe
Purva Patel <144143336+Purvapatel4725@users.noreply.github.com>
2024-12-16 15:44:10 -0500

    Update and rename Obsidian-As-Container to Obsidian-As-Container.sh

--- Obsidian-As-Container.sh
+++ Obsidian-As-Container.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
+#!/bin/bash
 
 docker run -d --name=obsidian --security-opt seccomp=unconfined -e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -p 3003:3000 -p 3004:3001 --shm-size="1gb" --restart unless-stopped lscr.io/linuxserver/obsidian:latest
