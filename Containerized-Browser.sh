bdbbf6a385b6c9dce4b2287fa1ce2d5630fdb2e2
Purva Patel <144143336+Purvapatel4725@users.noreply.github.com>
2024-12-19 13:59:43 -0500

    Delete Containerized-Browser.sh

--- Containerized-Browser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name=firefox --security-opt seccomp=unconfined -e PUID=1000 -e PGID=1000 -e TZ=Etc/UTC -e FIREFOX_CLI=https://purvapatel4725.github.io/purvapatel/ -p 5000:3000 --shm-size="1gb" --restart unless-stopped lscr.io/linuxserver/firefox:latest
